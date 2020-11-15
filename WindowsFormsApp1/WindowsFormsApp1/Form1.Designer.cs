36299560f5b9132b9a2325b3dc8a7121c369a857
dodinhnguyen <nguyendo891@yahoo.com>
2020-11-15 10:49:26 +0100

    Demo of how to use Task efficiency

---
+++ WindowsFormsApp1/WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,75 @@
+﻿
+namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.BtnCalculate = new System.Windows.Forms.Button();
+            this.LblResult = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // BtnCalculate
+            // 
+            this.BtnCalculate.Location = new System.Drawing.Point(363, 115);
+            this.BtnCalculate.Name = "BtnCalculate";
+            this.BtnCalculate.Size = new System.Drawing.Size(75, 23);
+            this.BtnCalculate.TabIndex = 0;
+            this.BtnCalculate.Text = "Calculate";
+            this.BtnCalculate.UseVisualStyleBackColor = true;
+            this.BtnCalculate.Click += new System.EventHandler(this.BtnCalculate_Click);
+            // 
+            // LblResult
+            // 
+            this.LblResult.AutoSize = true;
+            this.LblResult.Location = new System.Drawing.Point(388, 56);
+            this.LblResult.Name = "LblResult";
+            this.LblResult.Size = new System.Drawing.Size(35, 13);
+            this.LblResult.TabIndex = 1;
+            this.LblResult.Text = "label1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.LblResult);
+            this.Controls.Add(this.BtnCalculate);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button BtnCalculate;
+        private System.Windows.Forms.Label LblResult;
+    }
+}
+
---
+++ WindowsFormsApp1/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public async Task<int> CalculateValueAsync()
+        {
+            await Task.Delay(5000);
+            return 123;
+        }
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private async void BtnCalculate_Click(object sender, EventArgs e)
+        {
+            //var calculation = CalculateValueAsync();
+            //calculation.ContinueWith(t =>
+            //{
+            //    LblResult.Text = t.Result.ToString();
+
+            //}, TaskScheduler.FromCurrentSynchronizationContext());
+            int value = await CalculateValueAsync();
+            LblResult.Text = value.ToString();
+
+            await Task.Delay(5000);
+
+            using( var wc = new WebClient())
+            {
+                string data = await
+                    wc.DownloadStringTaskAsync("http://google.com/robots.txt");
+                LblResult.Text = data.Split('\n')[0].Trim();
+            }
+        }
+    }
+}
